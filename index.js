230d26c53fa6100975bfa1507e1251c03b209dad
simplysabir <simplysabir@gmail.com>
2023-06-08 13:20:49 +0530

    ✨ feat : Bot Login and Basic Interaction Added

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 import { Client, GatewayIntentBits } from 'discord.js';
-const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages] });
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
+const BOT_TOKEN = 'MTExNjI2NDc2OTYwMzc3NjU2Mg.GtDB1b.kA3ave7jp0fwFRNdDEtBXGph4fM2nFRnyjAqyg';
+// import { BOT_TOKEN } from './constants';
+
+client.on('messageCreate', message => {
+    if(message.author.bot) return;
+    message.reply({
+        content : "Hi From Bot",
+    })
+});
+
+client.login(BOT_TOKEN);
